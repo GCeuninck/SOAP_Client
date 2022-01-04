80ab035fd76570c8cacf62686daadf244729ddfb
GCeuninck <guillaume.ceuninck@etu.imt-lille-douai.fr>
2022-01-04 15:03:46 +0100

    Initial commit

---
+++ src/test/java/TemperatureSoapWebServiceConsumerTest.java
@@ -0,0 +1,20 @@
+import org.imt.nordeurope.j2ee.webservices.nickler.client.TemperatureSoapWebService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import javax.xml.namespace.QName;
+import javax.xml.ws.Service;
+import java.net.URL;
+
+public class TemperatureSoapWebServiceConsumerTest {
+    @Test
+    public void getTemperatureTest1() throws Exception {
+        URL url = new URL("http://localhost:9000/TemperatureSoapWebService?wsdl");
+        QName qname = new QName("http://nickler.webservices.j2ee.nordeurope.imt.org/", "TemperatureSoapWebService");
+        Service service = Service.create(url, qname);
+        TemperatureSoapWebService temperatureSoapWebService = service.getPort(TemperatureSoapWebService.class);
+        Integer temperature = temperatureSoapWebService.getTemperature("France", null);
+        Assertions.assertEquals(1, temperature);
+        System.out.println("TEMPERATURE_SOAP_CLIENT : Response from Server : getTemperature");
+    }
+}
