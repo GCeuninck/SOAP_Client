b529170a1fcb81905e396d0219877bfc7ba6e362
GCeuninck <guillaume.ceuninck@etu.imt-lille-douai.fr>
2022-01-08 15:09:37 +0100

    Test temperature depending on date

--- src/test/java/TemperatureSoapWebServiceConsumerTest.java
+++ src/test/java/TemperatureSoapWebServiceConsumerTest.java
@@ -2,9 +2,13 @@ import org.imt.nordeurope.j2ee.webservices.nickler.client.TemperatureSoapWebServ
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import javax.xml.datatype.DatatypeFactory;
+import javax.xml.datatype.XMLGregorianCalendar;
 import javax.xml.namespace.QName;
 import javax.xml.ws.Service;
 import java.net.URL;
+import java.time.LocalDate;
+import java.util.GregorianCalendar;
 
 public class TemperatureSoapWebServiceConsumerTest {
     @Test
@@ -13,7 +17,10 @@ public class TemperatureSoapWebServiceConsumerTest {
         QName qname = new QName("http://nickler.webservices.j2ee.nordeurope.imt.org/", "TemperatureSoapWebService");
         Service service = Service.create(url, qname);
         TemperatureSoapWebService temperatureSoapWebService = service.getPort(TemperatureSoapWebService.class);
-        Integer temperature = temperatureSoapWebService.getTemperature("France", null);
+
+        XMLGregorianCalendar today = DatatypeFactory.newInstance().newXMLGregorianCalendar(LocalDate.now().toString());
+
+        Integer temperature = temperatureSoapWebService.getTemperature("France", today);
         Assertions.assertEquals(1, temperature);
         System.out.println("TEMPERATURE_SOAP_CLIENT : Response from Server : getTemperature");
     }
